d5bbc31e2a1b9f38cf80a0be39991e8352b48294
sarlakZM <sarlak.zh@gmail.com>
2025-03-06 16:56:51 -0800

    Add Component Customer and store: ngrxsignals

--- projects/customer/src/app/app.component.ts
+++ projects/customer/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import {Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { CustomerComponent } from './pages/customer/customer.component';
 
 /**
  * @title Adding and removing data when using an array-based datasource.
  */
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CustomerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ projects/customer/src/app/store/customer.store.ts
@@ -0,0 +1,62 @@
+import { computed, inject } from '@angular/core';
+import {
+  patchState,
+  signalStore,
+  withComputed,
+  withMethods,
+  withProps,
+  withState,
+} from '@ngrx/signals';
+import { ICustomer } from '../models/customer.model';
+import { CustomerService } from '../services/customer.service';
+import { CustomersState } from '../models/store.model';
+
+
+const initialState: CustomersState = {
+    customers: [],
+    isLoading: false,
+};
+
+export const CustomersStore = signalStore(
+  { providedIn: 'root' },
+  withState(initialState),
+  withProps(() => ({
+    customerService: inject(CustomerService),
+  })),
+  withComputed(({ customers }) => ({
+    getCutomers: computed(() => customers()),
+    getCutomersEmails: computed(() => customers().map(customer => customer.email)),    
+  })),
+  withMethods(({ customerService, ...store }) => ({
+    async loadCustomers(): Promise<void> {
+      patchState(store, { isLoading: true });
+      const customers = customerService.getAll() ?? [];
+      patchState(store, { customers, isLoading: false });
+    },
+    updateCustomerByID( id: number, customer: ICustomer): void {  
+        patchState(store, { isLoading: true });
+        patchState(store, (state: CustomersState) => {
+          const customers = state.customers.map( item => item.id === id ? { id , ...customer} : item);
+          customerService.update(customers)
+          return { customers, isLoading: false}
+        });
+    },
+    AddCustomer( customer: ICustomer): void {  
+        patchState(store, { isLoading: true });
+        patchState(store,  ( state: CustomersState) => {
+            const customers = [...state.customers, {...customer, id: Date.now()}];
+            customerService.update(customers)
+           return  { customers: [...customers] , isLoading: false}
+        
+        });
+    },
+    removeCustomerByID( id: number): void {  
+        patchState(store, { isLoading: true });
+        patchState(store, (state: CustomersState) => {
+          const customers = state.customers.filter( item => item.id !== id);
+          customerService.update(customers)
+          return { customers, isLoading: false}
+        });
+    },
+  })),
+);
