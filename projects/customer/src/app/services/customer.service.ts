d19ca1a85ba6581542e9316b238d371888bd1106
sarlakZM <sarlak.zh@gmail.com>
2025-03-06 16:58:52 -0800

    Add Service Customer

---
+++ projects/customer/src/app/services/customer.service.ts
@@ -0,0 +1,21 @@
+import { inject, Injectable } from "@angular/core";
+import { LocalStorageService } from "../../../../shell/src/app/shared";
+import { ICustomer, ICustomerWithID } from "../models/customer.model";
+import { of } from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerService {
+  private localStorage = inject(LocalStorageService);
+  private key = 'customers'
+
+  getAll() {
+    return this.localStorage.get<ICustomerWithID[]>(this.key);
+  }
+
+  update(value: ICustomerWithID[] ){
+    return this.localStorage.set(this.key, value);
+
+  } 
+}
